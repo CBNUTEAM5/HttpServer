951f466dc354cb16062a71b45edf08a902475ebd
jenny <dpwb12@naver.com>
2024-11-27 13:14:50 +0900

    [feat] 멀티스레드 지원 추가로 클라이언트 요청 병렬 처리 구현 #3

--- server.c
+++ server.c
@@ -3,9 +3,35 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+
+// 클라이언트 요청을 처리하는 함수
+void *handle_client(void *arg) {
+    int client_socket = *(int *)arg;
+    free(arg);
+
+    // HTTP 응답 전송
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "\r\n"
+        "Hello, World!";
+    ssize_t bytes_written = write(client_socket, response, strlen(response));
+    if (bytes_written < 0) {
+        perror("Write failed");
+    } else {
+        printf("HTTP response sent to client.\n");
+    }
+
+    close(client_socket);
+    printf("Client connection closed.\n");
+
+    return NULL;
+}
 
 int main() {
     int server_socket;
@@ -41,31 +67,35 @@ int main() {
 
     printf("Server is running on port %d. Waiting for connections...\n", PORT);
 
-    // 클라이언트 연결 수락
-    int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (client_socket == -1) {
-        perror("Accept failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
+    while (1) {
+        // 클라이언트 연결 수락
+        int *client_socket = malloc(sizeof(int));
+        if (!client_socket) {
+            perror("Memory allocation failed");
+            continue;
+        }
+
+        *client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (*client_socket == -1) {
+            perror("Accept failed");
+            free(client_socket);
+            continue;
+        }
+
+        printf("Client connected.\n");
+
+        // 새로운 스레드 생성
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, handle_client, client_socket) != 0) {
+            perror("Thread creation failed");
+            close(*client_socket);
+            free(client_socket);
+            continue;
+        }
+
+        pthread_detach(thread_id); // 스레드가 종료되면 리소스를 자동 해제
     }
 
-    printf("Client connected.\n");
-
-    // HTTP 응답 전송
-    const char *response =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        "Content-Length: 13\r\n"
-        "\r\n"
-        "Hello, World!";
-    ssize_t bytes_written = write(client_socket, response, strlen(response));
-    if (bytes_written < 0) {
-        perror("Write failed");
-    } else {
-        printf("HTTP response sent to client.\n");
-    }
-
-    close(client_socket);
     close(server_socket);
 
     return 0;
